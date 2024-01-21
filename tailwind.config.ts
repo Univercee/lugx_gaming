7a1081fe05d3573c5780747b8203fdbad4fadedf
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-01-22 00:35:20 +0400

    update theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "accent": "#EE626A",
+        "primary": "#0171F8",
+        "secondary": "#008AF8"
+      }
     },
   },
   plugins: [],
