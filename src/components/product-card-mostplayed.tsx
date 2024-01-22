09217ecc921df948092b9b50d01da4e3ccc361d1
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-01-22 23:12:16 +0400

    css

--- src/components/product-card-mostplayed.tsx
+++ src/components/product-card-mostplayed.tsx
@@ -3,7 +3,7 @@ import { Game } from "@/lib/definitions";
 export default function ProductCardMostPlayed({data}: {data: Game}) {
     return (
       <div className="product-card bg-white rounded-3xl relative cursor-pointer flex flex-col items-center">
-        <img src={data.imageSrc} alt="Game image" className="rounded-3xl w-full" />
+        <img src={data.imageSrc} alt="Game image" className="rounded-3xl w-full aspect-square" />
         <div className="p-8 text-center">
             <p className="text-slate-500">{data.genres}</p>
             <p className="product-card-title font-semibold text-lg">{data.name}</p>
