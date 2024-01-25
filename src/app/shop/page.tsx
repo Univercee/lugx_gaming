c186fbe9fed3ef77cf2de6019dc95d7358c4f110
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-01-25 05:05:52 +0400

    trying to fix gh action build#9999

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,34 +1,34 @@
 import { BreadcrumbsBanner } from "@/components/breadcrumbs-banner";
-import ProductFilter from "@/components/product-filter";
 import ProductTable from "@/components/product-table";
 import Footer from "@/layout/footer";
 import Header from "@/layout/header";
+import { Game } from "@/lib/definitions";
 import { games } from "@/lib/placeholderdata";
 import { Suspense } from "react";
 
 export const dynamic = 'force-static'
 
-export default function Page({ searchParams }: {
+export default async function Page({ searchParams }: {
   searchParams?: {
       category?: string,
   };
 }) {
     const title = "Our shop";
-    const filteredGames = games.filter((game)=>{
-      if(searchParams?.category) return game.genres.includes(searchParams.category);
-      else return true;
-    })
+    const filteredGames = await (new Promise<Game[]>((resolve, reject)=>{
+      let data = games.filter((game)=>{
+        if(searchParams?.category) return game.genres.includes(searchParams.category);
+        else return true;
+      });
+      resolve(data);
+    }));
     
     return (
         <div className="min-h-screen flex flex-col gap-10">
           <Header Banner={<BreadcrumbsBanner title={title}/>}></Header>
           <div className="grow wrapper my-16">
-            <main className="flex flex-col justify-center items-center gap-32">
-              <ProductFilter></ProductFilter>
-              <Suspense>
-                <ProductTable games={filteredGames}></ProductTable>
-              </Suspense>
-            </main>
+            <Suspense>
+              <ProductTable games={filteredGames}></ProductTable>
+            </Suspense>
           </div>
           <Footer></Footer>
         </div>
--- src/components/product-table.tsx
+++ src/components/product-table.tsx
@@ -1,13 +1,18 @@
 import ProductCard from "@/components/product-card";
 import { Game } from "@/lib/definitions";
+import ProductFilter from "./product-filter";
+
 export default function ProductTable({games}:{
     games: Game[]
 }) {
     return (
-        <div className="grid gap-8 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
-            {games.map(game=>(
-            <ProductCard data={game}></ProductCard>
-            ))}
+        <div className="flex flex-col justify-center items-center gap-32">
+            <ProductFilter></ProductFilter>
+            <div className="grid gap-8 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+                {games.map(game=>(
+                <ProductCard data={game}></ProductCard>
+                ))}
+            </div>
         </div>
     );
   }
