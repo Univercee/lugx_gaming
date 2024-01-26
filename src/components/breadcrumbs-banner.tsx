962226ce7282910d60cb090af0425c3e0a73bc6d
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-01-26 04:42:18 +0400

    fix banner text aligment

--- src/components/breadcrumbs-banner.tsx
+++ src/components/breadcrumbs-banner.tsx
@@ -2,7 +2,7 @@ import { Breadcrumbs } from "./breadcrumbs"
 export function BreadcrumbsBanner({title, currentPageName}:{title: string, currentPageName?: string}){
     return (
         <div className="flex flex-col gap-4 justify-between items-center ">
-            <h1 className="uppercase font-semibold" style={{fontSize: "3rem"}}>{currentPageName?currentPageName:title}</h1>
+            <h1 className="uppercase font-semibold text-center" style={{fontSize: "3rem"}}>{currentPageName?currentPageName:title}</h1>
             <Breadcrumbs currentPageName={currentPageName}></Breadcrumbs>
         </div>
     )
