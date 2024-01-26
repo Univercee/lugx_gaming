caacd4f2f7df79571fd231938930a28376e98826
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-01-26 04:48:28 +0400

    update shop page

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -5,6 +5,7 @@ import { games } from "@/lib/placeholderdata";
 import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import Link from "next/link";
 
 export function generateStaticParams(){
   return [
@@ -27,8 +28,8 @@ export default function Page({params}: { params: {id: string}}) {
     <div className="min-h-screen flex flex-col">
       <Header Banner={<BreadcrumbsBanner title={params.id} currentPageName={game.name}/>}></Header>
       <div className="grow">
-        <main className="wrapper flex flex-col-reverse md:flex-row justify-center items-center gap-32 my-32">
-          <div className="w-full md:w-1/2">
+        <main className="wrapper flex flex-col-reverse md:flex-row justify-between items-center gap-32 my-32">
+          <div className="w-full md:w-2/3">
             <Image unoptimized src={game.imageSrc} alt={game.name} className="rounded-3xl aspect-square object-cover" width={2000} height={2000}></Image>
           </div>
           <div className="flex flex-col gap-8">
@@ -47,7 +48,9 @@ export default function Page({params}: { params: {id: string}}) {
             <div>
               <div className="flex gap-8">
                 <p className="text-gray-500">Genres: </p>
-                <p className="text-primary">{game.genres.join(", ")}</p>
+                <p className="text-primary hover:underline">{game.genres.map((genre)=>(
+                  <Link href={{pathname: "/shop", query: {category: genre}}}>{genre}</Link>
+                ))}</p>
               </div>
             </div>
           </div>
