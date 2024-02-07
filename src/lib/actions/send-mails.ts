8c1168e91b39016f20f228a05ac1d28d0ae4c13c
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-02-07 11:59:30 +0400

    update mail service

--- src/lib/actions/send-mails.ts
+++ src/lib/actions/send-mails.ts
@@ -1,36 +1,102 @@
 "use server"
-import { Resend } from "resend";
+import { Configuration, EmailsApi, EmailMessageData } from '@elasticemail/elasticemail-client-ts-axios';
 
-const resend = new Resend(process.env.RESENT_API_KEY);
+const config = new Configuration({
+    apiKey: process.env.ELASTIC_EMAIL_API_KEY
+});
+const emailsApi = new EmailsApi(config);
 
 //
 export async function sendVerificationEmail(email: string, token: string){
     const confirmLink = `${process.env.BASE_URL}/auth/new-verification?token=${token}`;
-    await resend.emails.send({
-        from: "onboarding@resend.dev",
-        to: email,
-        subject: "Confirm your email",
-        html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
+    const emailMessageData = {
+        Recipients: [
+          { 
+            Email: email,
+            Fields: {
+              link: confirmLink
+            }
+          }
+        ],
+        Content: {
+          Body: [
+            {
+              ContentType: "HTML",
+              Charset: "utf-8",
+              Content: "<p>Click <a href='{link}'>here</a> to verify email.</p>"
+            }
+          ],
+          From: process.env.MAIL_FROM,
+          Subject: "Your verification code"
+        }
+      } as EmailMessageData;
+    emailsApi.emailsPost(emailMessageData).then((response) => {
+        console.log('API called successfully.');
+        console.log(response.data);
+    }).catch((error) => {
+        console.error(error);
     });
 };
 
 //
 export async function sendResetPasswordEmail(email: string, token: string){
     const confirmLink = `${process.env.BASE_URL}/auth/new-password?token=${token}`;
-    await resend.emails.send({
-        from: "onboarding@resend.dev",
-        to: email,
-        subject: "Reset your password",
-        html: `<p>Click <a href="${confirmLink}">here</a> to reset password.</p>`
+    const emailMessageData = {
+        Recipients: [
+          { 
+            Email: email,
+            Fields: {
+              link: confirmLink
+            }
+          }
+        ],
+        Content: {
+          Body: [
+            {
+              ContentType: "HTML",
+              Charset: "utf-8",
+              Content: "<p>Click <a href='{confirmLink}'>here</a> to reset password.</p>"
+            }
+          ],
+          From: process.env.MAIL_FROM,
+          Subject: "Reset your password"
+        }
+      } as EmailMessageData;
+    emailsApi.emailsPost(emailMessageData).then((response) => {
+        console.log('API called successfully.');
+        console.log(response.data);
+    }).catch((error) => {
+        console.error(error);
     });
 };
 
 //
 export async function sendTwoFactorEmail(email: string, token: string){
-    await resend.emails.send({
-        from: "onboarding@resend.dev",
-        to: email,
-        subject: "2FA code",
-        html: `<p>Your 2FA code: ${token}</p>`
+    const emailMessageData = {
+        Recipients: [
+          { 
+            Email: email,
+            Fields: {
+              token: token
+            }
+          }
+        ],
+        Content: {
+          Body: [
+            {
+              ContentType: "HTML",
+              Charset: "utf-8",
+              Content: "<p>Your 2FA code: {token}</p>"
+            }
+          ],
+          From: process.env.MAIL_FROM,
+          Subject: "2FA code"
+        }
+      } as EmailMessageData;
+    emailsApi.emailsPost(emailMessageData).then((response) => {
+        console.log('API called successfully.');
+        console.log(response.data);
+    }).catch((error) => {
+        console.error(error);
     });
 };
