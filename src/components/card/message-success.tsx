1746a5def5f02ebfaa99bc51c0c7fd9eff8dc4cc
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-02-08 09:46:00 +0400

    update success message

--- src/components/card/message-success.tsx
+++ src/components/card/message-success.tsx
@@ -1,4 +1,4 @@
-import { faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle, faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useFormStatus } from "react-dom";
 
@@ -7,7 +7,7 @@ export function MessageSuccess({ message }: { message?: string|null }){
     const active = message && !pending;
     return (
         <div className={`flex items-center w-full p-4 rounded-xl bg-success/10 text-success ${active?"":"hidden"}`}>
-            <p className="font-medium"><FontAwesomeIcon icon={faXmarkCircle}></FontAwesomeIcon> {message}</p>
+            <p className="font-medium"><FontAwesomeIcon icon={faCheckCircle}></FontAwesomeIcon> {message}</p>
         </div>
     )
 }
