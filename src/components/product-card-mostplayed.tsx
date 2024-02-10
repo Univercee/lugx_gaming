23cea1f1eb765c8ab24b5f9b7ba59d6fa84e64fe
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2024-02-10 15:35:18 +0400

    css

--- src/components/product-card-mostplayed.tsx
+++ src/components/product-card-mostplayed.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function ProductCardMostPlayed({data}: {data: GameWithRelations}) {
     return (
       <Link href={{pathname: `shop/${data.id}`}} className="animated product-card bg-white rounded-3xl relative cursor-pointer flex flex-col items-center">
-        <img src={data.image} alt="Game image" className="rounded-3xl w-full aspect-square" />
+        <img src={data.image} alt="Game image" className="rounded-3xl w-full" style={{aspectRatio:"16/11"}} />
         <div className="p-8 text-center">
             <p className="text-slate-500">{data.genres.map((genre=>(genre.name))).join(" | ")}</p>
             <p className="product-card-title font-semibold text-lg">{data.name}</p>
